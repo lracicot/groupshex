f035e598a1aa1e1c984c31b9aebde0f712c994da
Louis Racicot <info@louisracicot.com>
2013-05-16 02:20:27 -0400

    add all models to the admin

--- src/expenses/admin.py
+++ src/expenses/admin.py
@@ -2,3 +2,6 @@ from models import *
 from django.contrib import admin
 
 admin.site.register(Expense)
+admin.site.register(Group)
+admin.site.register(Membership)
+admin.site.register(ExpensesUsers)
